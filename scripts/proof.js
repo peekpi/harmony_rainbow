05c0918d75e1a44bf9823f5ea9a63bebcfedae5f
Jacky Wang <jackyw.se@gmail.com>
2020-09-03 06:12:43 -0700

    added proof

--- scripts/proof.js
+++ scripts/proof.js
@@ -1,13 +1,33 @@
 const EthBridge = require('../lib/EthBridge');
-const contracts = require("../build/deployed.json");
+const testProverAddr = require("../misc/test.json").ProvethVerifier;
+const abiJson = require("../misc/test_abi.json");
+const rlp = require('rlp');
 
-const txHash = '0x9ebaff14e224d37deb0a559899f236c7b041aa373fe0456ab5da074c3d0f6ce3';
+const txHash = '0x2e75d7646b7239ceae67443268af9346f0fc0007edebe063ee35a86311a36ced';
 
 (async function() {
     const eb = new EthBridge()
+    const web3 = eb.web3
 
-    let header, receiptProof, txIndex = await eb.gp.receiptProof(txHash)
-    console.log(header)
-    console.log(receiptProof)
-    console.log(txIndex)
-} ()).catch( err => console.log )
+    try {
+        let resp = await eb.gp.receiptProof(txHash)
+        // let number = web3.utils.hexToNumber(web3.utils.bytesToHex(resp.header.number))
+        console.log(resp.txIndex)
+        // console.log(proof)
+
+        let receiptHash = web3.utils.bytesToHex(resp.header.receiptRoot)
+        // console.log("receipt hash", receiptHash)
+
+        let receiptRoot = web3.utils.bytesToHex(resp.header.receiptRoot)
+        let key = resp.txIndex
+        let receiptProof = web3.utils.bytesToHex(resp.receiptProof)
+
+        let testProver = new eb.web3.eth.Contract(abiJson.abi, testProverAddr)
+        let response = await testProver.methods.MPTProof(receiptRoot, key, receiptProof).call({gasLimit: 4712388000})
+
+        console.log(response)
+
+    } catch (e) {
+        console.log(e)
+    }
+} ()).catch( err => console.log ).finally(() => process.exit())
