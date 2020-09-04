92438e1029e91339c60f88a5ef0f1550afc87794
lixp <894646171@qq.com>
2020-09-04 22:33:21 +0800

    new contract

---
+++ contracts/LightClient.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+contract LightClient {
+    VerifyReceiptsHash(uint256 blockNo, bytes32 receiptsHash) view public return(bool) {
+        return true
+    }
+}
--- contracts/locker.sol
+++ contracts/locker.sol
@@ -1,21 +1,13 @@
-pragma solidity 0.5.17;
+pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-
 contract Locker {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
     IERC20 public ethToken_;
 
     mapping(bytes32 => bool) public usedEvents_;
-
-    event Locked(
-        address indexed token,
-        address indexed sender,
-        uint256 amount,
-        address recipient
-    );
 
     event Unlocked(uint256 amount, address recipient);
 
@@ -45,43 +37,5 @@ contract Locker {
         owner = msg.sender;
         wards[msg.sender] = 1;
         ethToken_ = ethToken;
-    }
-
-    /**
-     * @dev lock tokens to be minted on harmony chain
-     * @param recipient recipient address on the harmony chain
-     * @param amount amount of tokens to lock
-     */
-    function lockToken(address recipient, uint256 amount) public {
-        require(
-            recipient != address(0),
-            "Locker/recipient is a zero address"
-        );
-        require(amount > 0, "Locker/zero token locked");
-        uint256 _balanceBefore = ethToken_.balanceOf(msg.sender);
-        ethToken_.safeTransferFrom(msg.sender, address(this), amount);
-        uint256 _balanceAfter = ethToken_.balanceOf(msg.sender);
-        uint256 _actualAmount = _balanceBefore.sub(_balanceAfter);
-        emit Locked(address(ethToken_), msg.sender, _actualAmount, recipient);
-    }
-
-    /**
-     * @dev unlock tokens after burning them on harmony chain
-     * @param amount amount of unlock tokens
-     * @param recipient recipient of the unlock tokens
-     * @param receiptId transaction hash of the burn event on harmony chain
-     */
-    function unlockToken(
-        uint256 amount,
-        address recipient,
-        bytes32 receiptId
-    ) public auth {
-        require(
-            !usedEvents_[receiptId],
-            "Locker/The burn event cannot be reused"
-        );
-        usedEvents_[receiptId] = true;
-        ethToken_.safeTransfer(recipient, amount);
-        emit Unlocked(amount, recipient);
     }
 }
